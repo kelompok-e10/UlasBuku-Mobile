50a90e7d391ce80acfb6157c7c2d4db97db4d009
KronosDP <darrel.danadyaksa19@gmail.com>
2023-12-18 16:11:41 +0700

    merge fix

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,13 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
           title: 'Ulas Buku Mobile',
           theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(135, 148, 192, 1.0)),            useMaterial3: true,
+<<<<<<< HEAD
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Color.fromRGBO(135, 148, 192, 1.0)),
+            useMaterial3: true,
+=======
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(1, 1, 1, 0.8)),            useMaterial3: true,
+>>>>>>> 7c4a4f08a1f7e5896a93a9353b0cdc632e413a5a
           ),
           home: LoginPage()),
     );
