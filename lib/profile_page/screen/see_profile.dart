a922e8d3df5e4a7b19d3344fde25f9bf3d9c89a3
Dhemas25 <dhemaswicaksono@gmail.com>
2023-12-18 17:22:13 +0700

    profile page

--- lib/profile_page/screen/see_profile.dart
+++ lib/profile_page/screen/see_profile.dart
@@ -27,30 +27,93 @@ class _ProfilePageState extends State<ProfilePage> {
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       drawer: const LeftDrawer(),
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.all(16.0),
-          color: Colors.grey[200],
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
+      body: Column(
+        children: [
+          const Expanded(flex: 2, child: _TopPortion()),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    '${LoggedIn.user_data['username']!}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton.extended(
+                        onPressed: () {},
+                        heroTag: 'profile',
+                        elevation: 0,
+                        backgroundColor: Colors.red,
+                        label: const Text("Edit Profile Info"),
+                        icon: const Icon(Icons.description),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _TopPortion extends StatelessWidget {
+  const _TopPortion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 50),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [Color(0xff0043ba), Color(0xff006df1)]),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              )),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Stack(
+              fit: StackFit.expand,
               children: [
-                // Display the username
-                Text(
-                  '${LoggedIn.user_data['username']!}',
-                  style: TextStyle(
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
                     color: Colors.black,
-                    fontSize: 20.0,
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.person,
+                      size: 80,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                // Add other profile information or widgets as needed
               ],
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
